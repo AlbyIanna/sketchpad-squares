bf674dd1071178275f47c2b7452ac52da6d7c9f7
AlbyIanna <alby.ianna@gmail.com>
2017-06-05 15:24:13 +0200

    first version of sketchpad-squares

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	var container = $("#container");
+	for(i=0;i<16;i++){
+		var newRow = $("<div class='row' id='row-" + i + "'></div>");
+		container.append(newRow);
+		for(j=0;j<16;j++){
+			var newSquare = $("<div class='square' id='sq-" + j + "'></div>");
+			newRow.append(newSquare);
+			newSquare.hover(function() {
+				$(this).toggleClass("active");
+			}, function() {
+				$(this).addClass("hovered");
+			});
+		}
+	}
+	$("#btn-clear").on("click", function() {
+		value = prompt("Please, enter the number of squares per row:");
+		clear_and_rebuild(value);
+	});
+
+	console.log($("#btn-clear").text);
+	console.log("ciao");
+
+});
+
+function clear_and_rebuild(num) {
+	var container = $("#container");
+	container.empty();
+	$(".row").css("height", container.height()/num);
+
+	for(i=0;i<num;i++){
+		var newRow = $("<div class='row' id='row-" + i + "'></div>");
+		container.append(newRow);
+		for(j=0;j<num;j++){
+			var newSquare = $("<div class='square' id='sq-" + j + "'></div>");
+			newSquare.css("width", container.width()/num);
+			newSquare.css("height", container.height()/num);
+			newRow.append(newSquare);
+			newSquare.hover(function() {
+				$(this).toggleClass("active");
+			}, function() {
+				$(this).addClass("hovered");
+			});
+		}
+	}	
+}
